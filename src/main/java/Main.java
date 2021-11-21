466e2499e2c0f1445ec9e3d77f74b0d528e0a70e
lianakanukoeva <lianakanukoeva@yandex.ru>
2021-11-21 20:31:13 +0300

    created new project for SkillFactory

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Students student = new Students();
+        student.id = "id072415745";
+        student.fullName = "Antony Uly";
+        student.currentCourseNumber = 4;
+        student.universityId = "12id654654";
+        System.out.println(student.id + " " + student.fullName + " " + student.currentCourseNumber + " " + student.universityId);
+    }
+}
---
+++ src/main/java/Students.java
@@ -0,0 +1,70 @@
+public class Students {
+    String id;
+    String fullName;
+    String universityId;
+    int currentCourseNumber;
+    float avgExamScore;
+
+    public Students (String id, String fullName, String universityId, int currentCourseNumber, float avgExamScore) {
+        this.id = id;
+        this.fullName = fullName;
+        this.universityId = universityId;
+        this.currentCourseNumber = currentCourseNumber;
+        this.avgExamScore = avgExamScore;
+    }
+
+    public Students() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUniversityId() {
+        return universityId;
+    }
+
+    public int getCurrentCourseNumber() {
+        return currentCourseNumber;
+    }
+
+    public float getAvgExamScore() {
+        return avgExamScore;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setUniversityId(String universityId) {
+        this.universityId = universityId;
+    }
+
+    public void setCurrentCourseNumber(int currentCourseNumber) {
+        this.currentCourseNumber = currentCourseNumber;
+    }
+
+    public void setAvgExamScore(float avgExamScore) {
+        this.avgExamScore = avgExamScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", universityId='" + universityId + '\'' +
+                ", currentCourseNumber=" + currentCourseNumber +
+                ", avgExamScore=" + avgExamScore +
+                '}';
+    }
+}
