9921e9ec64a4edc95ecf480ad69ce20a9f0f7b47
lianakanukoeva <lianakanukoeva@yandex.ru>
2022-01-14 01:15:35 +0300

    added Emuns, Comparators and edit gitignor

---
+++ src/main/java/sorts/StudentsInterface.java
@@ -0,0 +1,8 @@
+package sorts;
+
+import model.Students;
+
+import java.util.Comparator;
+
+public interface StudentsInterface extends Comparator<Students> {
+}
---
+++ src/main/java/sorts/UniversityInterface.java
@@ -0,0 +1,8 @@
+package sorts;
+
+import model.University;
+
+import java.util.Comparator;
+
+public interface UniversityInterface extends Comparator<University> {
+}
